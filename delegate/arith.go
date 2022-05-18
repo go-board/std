e89c0e32426b695059f574eeda27309474cbb623
lixiaohui <lixiaohui.1024@bytedance.com>
2022-05-18 13:08:40 +0800

    [delegate] deprecated, use raw func or fp instead

--- delegate/arith.go
+++
@@ -1,6 +0,0 @@
-package delegate
-
-type Add[Lhs, Rhs, Output any] Function2[Lhs, Rhs, Output]
-type Sub[Lhs, Rhs, Output any] Function2[Lhs, Rhs, Output]
-type Mul[Lhs, Rhs, Output any] Function2[Lhs, Rhs, Output]
-type Div[Lhs, Rhs, Output any] Function2[Lhs, Rhs, Output]
